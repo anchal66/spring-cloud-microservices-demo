bf2f7daeef71b0257960741649284bf6ce21e148
Avinash Anchal <anchalavinash66@gmail.com>
2021-06-04 19:25:01 +0530

    Created a microservice client Limit Service

---
+++ limits-service/src/main/java/com/anchal/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,14 @@
+package com.anchal.microservices.limitsservice.controller;
+
+import com.anchal.microservices.limitsservice.dto.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits listLimit(){
+        return new Limits(1, 1000);
+    }
+}
