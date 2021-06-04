d90433102554f68cb52275841e780f8c17a6a47d
Avinash Anchal <anchalavinash66@gmail.com>
2021-06-04 19:32:47 +0530

    Get values from properties file

--- limits-service/src/main/java/com/anchal/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/anchal/microservices/limitsservice/controller/LimitsController.java
@@ -1,14 +1,21 @@
 package com.anchal.microservices.limitsservice.controller;
 
+import com.anchal.microservices.limitsservice.config.Configuration;
 import com.anchal.microservices.limitsservice.dto.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits listLimit(){
-        return new Limits(1, 1000);
+//        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
+
 }
