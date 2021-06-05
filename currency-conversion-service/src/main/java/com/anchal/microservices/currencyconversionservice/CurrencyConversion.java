44bb52d8b0c6c1119c852a14a66690e97b0957a2
Avinash Anchal <anchalavinash66@gmail.com>
2021-06-05 20:05:25 +0530

    calling other microservice using RestTemplate

--- currency-conversion-service/src/main/java/com/anchal/microservices/currencyconversionservice/CurrencyConversion.java
+++ currency-conversion-service/src/main/java/com/anchal/microservices/currencyconversionservice/CurrencyConversion.java
@@ -14,8 +14,8 @@ public class CurrencyConversion {
     private Long id;
     private String from;
     private String to;
-    private BigDecimal conversionMultiple;
     private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
     private BigDecimal totalCalcAmt;
     private String environment;
 }
--- currency-conversion-service/src/main/java/com/anchal/microservices/currencyconversionservice/CurrencyConversonController.java
+++ currency-conversion-service/src/main/java/com/anchal/microservices/currencyconversionservice/CurrencyConversonController.java
@@ -1,10 +1,13 @@
 package com.anchal.microservices.currencyconversionservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversonController {
@@ -12,6 +15,20 @@ public class CurrencyConversonController {
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calcCurrency(@PathVariable String from, @PathVariable String to,
                                            @PathVariable BigDecimal quantity){
-        return null;
+
+        HashMap<String, String> uriVariables = new HashMap<>() ;
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        String url = "http://localhost:8000/currency-exchange/from/{from}/to/{to}";
+        ResponseEntity<CurrencyConversion> responseEntity =
+                new RestTemplate().getForEntity(url, CurrencyConversion.class, uriVariables);
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from, to, quantity,
+                BigDecimal.ONE, BigDecimal.ONE,
+                currencyConversion.getEnvironment()
+        );
     }
 }
