7be94f5a915b03ffa31e42b47081dcecb42499d8
Avinash Anchal <anchalavinash66@gmail.com>
2021-06-05 21:03:58 +0530

    Adding load balacer

--- currency-conversion-service/src/main/java/com/anchal/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/anchal/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+
+//just By removing url it will automatically
+//talk to naming server and ask for utl on basis of service name
+
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
