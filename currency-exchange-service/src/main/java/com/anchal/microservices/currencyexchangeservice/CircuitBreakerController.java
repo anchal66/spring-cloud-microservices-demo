1113ab09b83c5525a3b78244033a1b544a28bb1c
Avinash Anchal <anchalavinash66@gmail.com>
2021-06-06 15:05:50 +0530

    Playing with Resilience4J Retry and FallbackMethods

---
+++ currency-exchange-service/src/main/java/com/anchal/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,25 @@
+package com.anchal.microservices.currencyexchangeservice;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    @GetMapping("/sample-api")
+//    @Retry(name = "default")
+    @Retry(name = "sample-api", fallbackMethod = "failedMessageRes")
+    public String sampleAPi(){
+        System.out.println("trying");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://dummylink:8080/dum", String.class);
+
+        return forEntity.getBody();
+    }
+
+    public String failedMessageRes(Exception exception){
+        return "Failed sorry " + exception.getMessage();
+    }
+}
